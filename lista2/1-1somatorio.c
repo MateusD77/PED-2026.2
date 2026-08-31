ab17835a3eaf34db3ebeddf2e194192d9fafa19f
Mateus Dourado <166462181+MateusD77@users.noreply.github.com>
2026-08-31 20:21:19 -0300

    Add somatorio function to calculate sum of numbers

---
+++ lista2/1-1somatorio.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int somatorio(int n) {
+    if (n == 1) {      // caso base
+        return 1;
+    }
+
+    return n + somatorio(n - 1);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", somatorio(n));
+
+    return 0;
+}
