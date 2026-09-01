6ec716d7022dafd485194a1756793bd3a984f8a5
Mateus Dourado <166462181+MateusD77@users.noreply.github.com>
2026-09-01 12:23:29 -0300

    Update 1-1somatorio.c

--- lista2/1-1somatorio.c
+++ lista2/1-1somatorio.c
@@ -1,30 +1,28 @@
 /********************
-retornando ao somatorio(n) = 28 
-28
-27 + (2 - 1)
-25 + (3 - 1)
-22 + (4 - 1)
-18 + (5 - 1)
-13 + (6 - 1)
-somatorio = 7 + (7 - 1)
-n = 7
+soma(1) -> return 1
+soma(2) -> return 2 + soma(1) = 2 + 1 
+soma(3) -> return 3 + soma(2) = 3 + 3
+soma(4) -> return 4 + soma(3) = 6 + 4
+soma(5) -> return 5 + soma(4) = 10 + 5
+soma(6) -> return 6 + soma(5) = 15 + 6
+soma(7) -> return 7 + soma(6) = 21 + 7
 ******************/
 
 #include <stdio.h>
 
-int somatorio(int n) {
+int soma(int n) {
     if (n == 1) {      // caso base
         return 1;
     }
 
-    return n + somatorio(n - 1);
+    return n + soma(n - 1);
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    printf("%d\n", somatorio(n));
+    printf("%d\n", soma(n));
 
     return 0;
 }
